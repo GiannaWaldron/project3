c6acfbc28d29dcbcda3e5df0897f4f0faae72e41
GiannaWaldron <waldg352@newschool.edu>
2022-04-19 01:01:44 -0400

    fire type

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-var imageArray = new Array(); 
-imageArray[0] = "1.png"; 
-imageArray[1] = "2.png"; 
-imageArray[2] = "3.png"; 
- 
-document.getElementById("myButton").addEventListener("click", onClick); 
- 
-function onClick() 
-{ 
-	console.log(Math.floor(Math.random() * imageArray.length)); 
-} 
+var imgTimer;
+
+function imageTimeOut(){
+    imgTimer = setTimeout(getRandomImage, 1000);   
+}
+function getRandomImage() {
+    var randomImage = new Array();
+    randomImage[0] = "image/1.png";
+    randomImage[1] = "image/2.png";
+    randomImage[2] = "image/3.png";
+    randomImage[3] = "image/4.png";
+    randomImage[4] = "image/5.png";
+    randomImage[5] = "image/6.png";
+    randomImage[6] = "image/7.png";
+    randomImage[7] = "image/8.png";
+    randomImage[8] = "image/9.png";
+    randomImage[9] = "image/10.png";
+    randomImage[10] = "image/11.png";
+    randomImage[11] = "image/12.png";
+	randomImage[11] = "image/13.png";
+
+    for (let i = 0; i < 13; i++) {
+        var number = Math.floor(Math.random() * randomImage.length);
+        document.getElementById("result").innerHTML = '<img src="' + randomImage[number] + '" style="margin-right=300px" />';
+    }
+}
